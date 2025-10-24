f3cb999aa236d6e3fa8a3de58f5df878c025ce67
Leonel Torres <leo_torres089@outlook.com>
2025-10-24 15:28:49 -0300

    feat: add SecondaryHeader component to InventoryPage for improved structure

--- src/po/pages/inventory.page.js
+++ src/po/pages/inventory.page.js
@@ -1,11 +1,13 @@
 const BasePage = require('./base.page');
 const Header = require('../components/common/header.component');
+const SecondaryHeader = require('../components/common/secondary.header.component');
 
 class InventoryPage extends BasePage {
 
     constructor() {
         super("inventory.html");
         this.header = new Header();
+        this.secondaryHeader = new SecondaryHeader();
     }
 
     get title () {
